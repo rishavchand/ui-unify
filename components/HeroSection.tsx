1a032f6d6ac74485cf16b624ede8c433439499fe
pankaj kumar <pankajamsali@gmail.com>
2024-12-19 23:59:45 +0530

    feat: update Hero component layout and remove Spotlight component; add utility function for class name merging

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,9 +1,30 @@
-import { Spotlight } from "./Spotlight";
+
 
 export function Hero() {
-    return(
-        <div>
-        <Spotlight/>
+  return (
+    <div>
+      <div className="flex justify-center items-center mt-16">
+      
+      </div>
+      <div className="flex justify-center items-center h-full mt-8">
+        <div className="flex flex-col items-center">
+          <h1 className="text-6xl font-bold bg-gradient-to-r from-orange-400 to-orange-700 text-transparent bg-clip-text">
+            Multiple UI <span className="text-gray-400">Libraries,</span>
+          </h1>
+          <h1 className="text-6xl font-bold bg-gradient-to-r from-orange-400 to-orange-700 text-transparent bg-clip-text">
+            One <span>Platform,</span> Simplified
+          </h1>
+
+          <p className="mt-8 text-xl font-medium dark:text-slate-200 text-slate-500">
+          Bring together the best components from multiple UI libraries,
+
+          </p>
+          <p className="text-xl font-medium dark:text-slate-200 text-slate-500">
+          making development faster, easier, and more efficient.
+          </p>
         </div>
-    )
+        
+      </div>
+    </div>
+  );
 }
