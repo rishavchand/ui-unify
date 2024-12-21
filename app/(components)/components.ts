b199204979e64dd94f76293e9076d7407ed3bcd5
pankaj kumar <pankajamsali@gmail.com>
2024-12-22 00:25:22 +0530

    feat: add new components (Button Group, Card, Carousel, Collapse, Dropdown, Empty, Form, Form Item, Grid, Icon, Input, Input Number) to the components list

--- app/(components)/components.ts
+++ app/(components)/components.ts
@@ -40,6 +40,55 @@ export const components = [
         label: 'Button',
         Link: '/components/Button',
       },
+      {
+        label: 'Button Group',
+        Link: '/components/ButtonGroup',
+        },
+        {
+            label: 'Card',
+            Link: '/components/Card',
+        },
+        {
+            label: 'Carousel',
+            Link: '/components/Carousel',
+        },
+        {
+            label: 'Collapse',
+            Link: '/components/Collapse',
+        },
+        {
+            label: 'Dropdown',
+            Link: '/components/Dropdown',
+        },
+        {
+            label: 'Empty',
+            Link: '/components/Empty',
+        },
+        {
+            label: 'Form',
+            Link: '/components/Form',
+        },
+        {
+            label: 'Form Item',
+            Link: '/components/FormItem',
+        },
+        {
+            label: 'Grid',
+            Link: '/components/Grid',
+        },
+        {
+            label: 'Icon',
+            Link: '/components/Icon',
+        },
+        {
+            label: 'Input',
+            Link: '/components/Input',
+        },
+        {
+            label: 'Input Number',
+            Link: '/components/InputNumber',
+        },
+        
     ],
   },
 ];
