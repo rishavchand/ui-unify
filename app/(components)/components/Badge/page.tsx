319607b0b19b7b00c675e671dbc6687429ee01f2
pankaj kumar <pankajamsali@gmail.com>
2024-12-26 00:02:55 +0530

    refactor: remove unused imports in Badge component page

--- app/(components)/components/Badge/page.tsx
+++ app/(components)/components/Badge/page.tsx
@@ -1,7 +1,5 @@
 import FullComponent from '@/components/FullComponent';
 import { CodeBlockDemo } from '@/components/ui/Codeblock';
-import { Snippet } from '@nextui-org/snippet';
-import { FaTerminal } from 'react-icons/fa';
 import { BadgeDemo } from '../preview/BadgeDemo';
 
 export default function page() {
