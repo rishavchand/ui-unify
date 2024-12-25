de2289543a61b33b5310e4357647bb74ee109a5a
pankaj kumar <pankajamsali@gmail.com>
2024-12-25 23:52:02 +0530

    feat: add Badge component with demo and usage instructions

---
+++ app/(components)/components/Badge/page.tsx
@@ -0,0 +1,87 @@
+import FullComponent from '@/components/FullComponent';
+import { CodeBlockDemo } from '@/components/ui/Codeblock';
+import { Snippet } from '@nextui-org/snippet';
+import { FaTerminal } from 'react-icons/fa';
+import { BadgeDemo } from '../preview/BadgeDemo';
+
+export default function page() {
+  return (
+    <div>
+      <FullComponent
+        heading="Badge"
+        code={`import { Badge } from "@/components/ui/badge"
+
+export function BadgeDemo() {
+  return <Badge>Badge</Badge>
+}
+`}
+        description="Displays a badge or a component that looks like a badge."
+        library="Shadcn/ui"
+        filename="Badge.tsx"
+        cli={`npx shadcn@latest add badge`}
+        Tab1={<BadgeDemo />}
+        child={
+          <>
+            <div>
+              <div className="flex gap-2 mt-4 ">
+                <div className="w-6 h-6 rounded-full bg-gray-800 dark:bg-gray-200 flex items-center justify-center text-white dark:text-black">
+                  1
+                </div>
+                <div>Copy and paste the following code into your project.</div>
+              </div>
+              <div>
+                <div className="overflow-x-auto mt-2 h-96">
+                  <CodeBlockDemo
+                    code={`import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+        secondary:
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        destructive:
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        outline: "text-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  )
+}
+
+export { Badge, badgeVariants }
+`}
+                  />
+                </div>
+              </div>
+              <div className="flex gap-2 mt-4">
+                <div className="w-6 h-6 rounded-full bg-gray-800 dark:bg-gray-200 flex items-center justify-center text-white dark:text-black">
+                  3
+                </div>
+                <div>Update the import paths to match your project setup.</div>
+              </div>
+            </div>
+          </>
+        }
+      />
+    </div>
+  );
+}
