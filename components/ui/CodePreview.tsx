97f23a3a374d9918d1e06357d1bb401367f4ae23
pankaj kumar <pankajamsali@gmail.com>
2024-12-25 23:38:38 +0530

    feat: swap icons and labels for Code and Preview tabs in CodePreview component

--- components/ui/CodePreview.tsx
+++ components/ui/CodePreview.tsx
@@ -14,15 +14,15 @@ export function CodePreview({ tab1Content, tab2Content }: TabsProps) {
           value="Code"
           className="px-4 py-2 border-b-2 border-transparent data-[state=active]:border-b-blue-500 dark:data-[state=active]:border-b-blue-400 data-[state=active]:rounded-none flex items-center"
         >
-          <FaCode className="mr-2" />
-          Code
+          <FaEye className="mr-2" />
+          Preview
         </TabsTrigger>
         <TabsTrigger
           value="Preview"
           className="px-4 py-2 data-[state=active]:border-b-2 data-[state=active]:border-b-blue-500 dark:data-[state=active]:border-b-blue-400 data-[state=active]:rounded-none flex items-center"
         >
-          <FaEye className="mr-2" />
-          Preview
+          <FaCode className="mr-2" />
+          Code
         </TabsTrigger>
       </TabsList>
       <div className="mt-8 border max-h-96 border-gray-300 dark:border-gray-700 rounded-lg ">
