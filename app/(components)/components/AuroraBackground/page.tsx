be876457e95394ab9429d09d657c9ac0e40f0b72
pankaj kumar <pankajamsali@gmail.com>
2024-12-27 21:28:10 +0530

    feat: enhance AuroraBackgroundDemo with improved code examples and layout adjustments

--- app/(components)/components/AuroraBackground/page.tsx
+++ app/(components)/components/AuroraBackground/page.tsx
@@ -47,11 +47,14 @@ export function AuroraBackgroundDemo() {
         filename="AuroraBackground.tsx"
         cli={`npx shadcn@latest add https://ui.aceternity.com/registry/aurora-background.json`}
         Tab1={<AnimatedPinDemo />}
-        tab1Extra = {<>
-        <div className='mt-4'>Modify the config file to add animation and colors as variables</div>
-        <div className=''>
-            <CodeBlockDemo 
-            code= {`const {
+        tab1Extra={
+          <>
+            <div className="mt-4">
+              Modify the config file to add animation and colors as variables
+            </div>
+            <div className="overflow-x-auto max-h-96 mt-2">
+              <CodeBlockDemo
+                code={`const {
     default: flattenColorPalette,
   } = require("tailwindcss/lib/util/flattenColorPalette");
   
@@ -94,12 +97,15 @@ export function AuroraBackgroundDemo() {
     });
   }
   `}
-  filename='AuroraBackground.tsx'
-  highlightLines={[1,2,3,15,18,19,20,21,22,23,24,25,29,33,34,35,36,37,38,39,40,41,42]}
-
-            />
-        </div>
-        </>}
+                filename="tailwind.config.js"
+                highlightLines={[
+                  1, 2, 3, 15, 18, 19, 20, 21, 22, 23, 24, 25, 29, 33, 34, 35,
+                  36, 37, 38, 39, 40, 41, 42,
+                ]}
+              />
+            </div>
+          </>
+        }
         child={
           <>
             <div>
@@ -138,189 +144,144 @@ export function cn(...inputs: ClassValue[]) {
                   />
                 </div>
               </div>
+              <div>
+                <div>
+                  <div className="flex gap-2 mt-4 ">
+                    <div className="w-6 h-6 rounded-full bg-gray-800 dark:bg-gray-200 flex items-center justify-center text-white dark:text-black">
+                      3
+                    </div>
+                    <div>
+                      {' '}
+                      Modify the config file to add animation and colors as
+                      variables.
+                    </div>              
+                  </div>
+                </div>
+                <div>
+                <div className="overflow-x-auto max-h-96 mt-2">
+              <CodeBlockDemo
+                code={`const {
+    default: flattenColorPalette,
+  } = require("tailwindcss/lib/util/flattenColorPalette");
+  
+  /** @type {import('tailwindcss').Config} */
+  module.exports = {
+    content: [
+      // your paths
+      "./src/**/*.{ts,tsx}",
+    ],
+    darkMode: "class",
+    theme: {
+      extend: {
+        animation: {
+          aurora: "aurora 60s linear infinite",
+        },
+        keyframes: {
+          aurora: {
+            from: {
+              backgroundPosition: "50% 50%, 50% 50%",
+            },
+            to: {
+              backgroundPosition: "350% 50%, 350% 50%",
+            },
+          },
+        },
+      },
+    },
+    plugins: [addVariablesForColors],
+  };
+  
+  // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+  function addVariablesForColors({ addBase, theme }: any) {
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(
+      Object.entries(allColors).map(([key, val]) => [\`--\${key}\`, val])
+    );
+  
+    addBase({
+      ":root": newVars,
+    });
+  }
+  `}
+                filename="tailwind.config.js"
+                highlightLines={[
+                  1, 2, 3, 15, 18, 19, 20, 21, 22, 23, 24, 25, 29, 33, 34, 35,
+                  36, 37, 38, 39, 40, 41, 42,
+                ]}
+              />
+            </div>
+                </div>
+              </div>
               <div className="flex gap-2 mt-4 ">
                 <div className="w-6 h-6 rounded-full bg-gray-800 dark:bg-gray-200 flex items-center justify-center text-white dark:text-black">
-                  3
+                  4
                 </div>
                 <div>Copy and paste the following code into your project.</div>
               </div>
               <div>
                 <div className="overflow-x-auto mt-2 h-96">
                   <CodeBlockDemo
-                    code={`"use client";
-import React, { useState } from "react";
-import { motion } from "framer-motion";
+                  code = {`"use client";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
+import React, { ReactNode } from "react";
 
-export const PinContainer = ({
-  children,
-  title,
-  href,
+interface AuroraBackgroundProps extends React.HTMLProps<HTMLDivElement> {
+  children: ReactNode;
+  showRadialGradient?: boolean;
+}
+
+export const AuroraBackground = ({
   className,
-  containerClassName,
-}: {
-  children: React.ReactNode;
-  title?: string;
-  href?: string;
-  className?: string;
-  containerClassName?: string;
-}) => {
-  const [transform, setTransform] = useState(
-    "translate(-50%,-50%) rotateX(0deg)"
-  );
-
-  const onMouseEnter = () => {
-    setTransform("translate(-50%,-50%) rotateX(40deg) scale(0.8)");
-  };
-  const onMouseLeave = () => {
-    setTransform("translate(-50%,-50%) rotateX(0deg) scale(1)");
-  };
-
+  children,
+  showRadialGradient = true,
+  ...props
+}: AuroraBackgroundProps) => {
   return (
-    <Link
-      className={cn(
-        "relative group/pin z-50  cursor-pointer",
-        containerClassName
-      )}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      href={href || "/"}
-    >
+    <main>
       <div
-        style={{
-          perspective: "1000px",
-          transform: "rotateX(70deg) translateZ(0deg)",
-        }}
-        className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
+        className={cn(
+          "relative flex flex-col  h-[100vh] items-center justify-center bg-zinc-50 dark:bg-zinc-900  text-slate-950 transition-bg",
+          className
+        )}
+        {...props}
       >
-        <div
-          style={{
-            transform: transform,
-          }}
-          className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-black border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
-        >
-          <div className={cn(" relative z-50 ", className)}>{children}</div>
+        <div className="absolute inset-0 overflow-hidden">
+          <div
+            //   I'm sorry but this is what peak developer performance looks like // trigger warning
+            className={cn(
+              \`
+            [--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]
+            [--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]
+            [--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)]
+            [background-image:var(--white-gradient),var(--aurora)]
+            dark:[background-image:var(--dark-gradient),var(--aurora)]
+            [background-size:300%,_200%]
+            [background-position:50%_50%,50%_50%]
+            filter blur-[10px] invert dark:invert-0
+            after:content-[""] after:absolute after:inset-0 after:[background-image:var(--white-gradient),var(--aurora)] 
+            after:dark:[background-image:var(--dark-gradient),var(--aurora)]
+            after:[background-size:200%,_100%] 
+            after:animate-aurora after:[background-attachment:fixed] after:mix-blend-difference
+            pointer-events-none
+            absolute -inset-[10px] opacity-50 will-change-transform\`,
+
+              showRadialGradient &&
+                \`[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]\`
+            )}
+          ></div>
         </div>
+        {children}
       </div>
-      <PinPerspective title={title} href={href} />
-    </Link>
-  );
-};
-
-export const PinPerspective = ({
-  title,
-  href,
-}: {
-  title?: string;
-  href?: string;
-}) => {
-  return (
-    <motion.div className="pointer-events-none  w-96 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
-      <div className=" w-full h-full -mt-7 flex-none  inset-0">
-        <div className="absolute top-0 inset-x-0  flex justify-center">
-          <a
-            href={href}
-            target={"_blank"}
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
-          >
-            <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
-              {title}
-            </span>
-
-            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
-        </div>
-
-        <div
-          style={{
-            perspective: "1000px",
-            transform: "rotateX(70deg) translateZ(0)",
-          }}
-          className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
-        >
-          <>
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0,
-                x: "-50%",
-                y: "-50%",
-              }}
-              animate={{
-                opacity: [0, 1, 0.5, 0],
-                scale: 1,
-
-                z: 0,
-              }}
-              transition={{
-                duration: 6,
-                repeat: Infinity,
-                delay: 0,
-              }}
-              className="absolute left-1/2 top-1/2  h-[11.25rem] w-[11.25rem] rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]"
-            ></motion.div>
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0,
-                x: "-50%",
-                y: "-50%",
-              }}
-              animate={{
-                opacity: [0, 1, 0.5, 0],
-                scale: 1,
-
-                z: 0,
-              }}
-              transition={{
-                duration: 6,
-                repeat: Infinity,
-                delay: 2,
-              }}
-              className="absolute left-1/2 top-1/2  h-[11.25rem] w-[11.25rem] rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]"
-            ></motion.div>
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0,
-                x: "-50%",
-                y: "-50%",
-              }}
-              animate={{
-                opacity: [0, 1, 0.5, 0],
-                scale: 1,
-
-                z: 0,
-              }}
-              transition={{
-                duration: 6,
-                repeat: Infinity,
-                delay: 4,
-              }}
-              className="absolute left-1/2 top-1/2  h-[11.25rem] w-[11.25rem] rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]"
-            ></motion.div>
-          </>
-        </div>
-
-        <>
-          <motion.div className="absolute right-1/2 bottom-1/2 bg-gradient-to-b from-transparent to-cyan-500 translate-y-[14px] w-px h-20 group-hover/pin:h-40 blur-[2px]" />
-          <motion.div className="absolute right-1/2 bottom-1/2 bg-gradient-to-b from-transparent to-cyan-500 translate-y-[14px] w-px h-20 group-hover/pin:h-40  " />
-          <motion.div className="absolute right-1/2 translate-x-[1.5px] bottom-1/2 bg-cyan-600 translate-y-[14px] w-[4px] h-[4px] rounded-full z-40 blur-[3px]" />
-          <motion.div className="absolute right-1/2 translate-x-[0.5px] bottom-1/2 bg-cyan-300 translate-y-[14px] w-[2px] h-[2px] rounded-full z-40 " />
-        </>
-      </div>
-    </motion.div>
+    </main>
   );
 };
 `}
-                    filename="3DPin.tsx"
                   />
                 </div>
               </div>
               <div className="flex gap-2 mt-4">
                 <div className="w-6 h-6 rounded-full bg-gray-800 dark:bg-gray-200 flex items-center justify-center text-white dark:text-black">
-                  4
+                  5
                 </div>
                 <div>Update the import paths to match your project setup.</div>
               </div>
