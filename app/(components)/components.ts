bc10828e57d48da4330264467554f05608b5648e
pankaj kumar <pankajamsali@gmail.com>
2024-12-27 14:10:26 +0530

    fix: update link for 3D Card Effect component to point to the correct path

--- app/(components)/components.ts
+++ app/(components)/components.ts
@@ -22,7 +22,7 @@ export const components = [
     children: [
       {
         label: '3D Card Effect',
-        Link: '/components/3dCardEffect',
+        Link: '/components/3dCard',
       },
       {
         label: 'Accordion',
