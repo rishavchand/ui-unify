c6ae2519fb00e9eb34c33cc538846b91b54cc0db
pankaj kumar <pankajamsali@gmail.com>
2025-05-02 20:23:10 +0530

    fix: adjust container class in Navigation component for consistent layout

--- components/Navgation.tsx
+++ components/Navgation.tsx
@@ -48,7 +48,7 @@ export function Navigation() {
 
   return (
     <nav className="sticky top-0 z-50 w-full  bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-6">
-      <div className="container flex h-16 items-center justify-between">
+      <div className=" flex h-16 items-center justify-between w-full">
         <div className="flex items-center gap-6">
           <Link href="/" className="flex items-center space-x-2">
             <ChartNoAxesGantt />
